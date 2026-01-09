9a76136da91c9c2b6c5f105d3d3b5db96d8430a9
Aiden Ghim <aiden.bj.ghim@gmail.com>
2026-01-09 23:32:33 +0900

    Update emacs whatever

---
+++ .emacs.d/elpa/swift-mode-9.4.0/swift-mode-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from swift-mode.el  -*- no-byte-compile: t -*-
+(define-package "swift-mode" "9.4.0" "Major-mode for Apple's Swift programming language" '((emacs "24.4") (seq "2.3")) :commit "fc7df7bd906a2bb04aac6e0de47fc7acf33ceed3" :authors '(("taku0" . "mxxouy6x3m_github@tatapa.org") ("Chris Barrett" . "chris.d.barrett@me.com") ("Bozhidar Batsov" . "bozhidar@batsov.com") ("Arthur Evstifeev" . "lod@pisem.net")) :maintainer '("taku0" . "mxxouy6x3m_github@tatapa.org") :keywords '("languages" "swift") :url "https://github.com/swift-emacs/swift-mode")
